185052c44a150f5ad9ab5762d95f914668268eac
Ninjateen <vinaychaudhari02@gmail.com>
2023-05-02 14:23:27 +0530

    app

--- app.py
+++ app.py
@@ -1,44 +1,71 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
+application = Flask(__name__)
 
-app=application
+app = application
 
-## Route for a home page
-
+# Route for a home page
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData(
+        data = CustomData(
             Source=request.form.get('Source'),
             Destination=request.form.get('Destination'),
             Total_Stops=request.form.get('Total_Stops'),
-            Date_of_Journey=request.form.get('Date_of_Journey'),
-            #Airline=request.form.get('Airline'),
-        
+            day_of_journey=request.form.get('day_of_journey'),
+            month_of_journey=request.form.get('month_of_journey'),
+            Dep_Hour=request.form.get('Dep_Hour'),
+            Dep_Min=request.form.get('Dep_Min'),
+            Duration_Total_Hour=request.form.get('Duration_Total_Hour'),
+            Arrival_Hour=request.form.get('Arrival_Hour'),
+            Arrival_Min=request.form.get('Arrival_Min'),
+            Airline_Air_Asia=request.form.get('Airline_Air_Asia'),
+            Airline_Air_India=request.form.get('Airline_Air India'),
+            Airline_GoAir=request.form.get('Airline_GoAir'),
+            Airline_IndiGo=request.form.get('Airline_IndiGo'),
+            Airline_Jet_Airways=request.form.get('Airline_Jet Airways'),
+            Airline_Jet_Airways_Business=request.form.get('Airline_Jet Airways Business'),
+            Airline_Multiple_carriers=request.form.get('Airline_Multiple carriers'),
+            Airline_Multiple_carriers_Premium_economy=request.form.get('Airline_Multiple carriers Premium economy'),
+            Airline_SpiceJet=request.form.get('Airline_SpiceJet'),
+            Airline_Trujet=request.form.get('Airline_Trujet'),
+            Airline_Vistara=request.form.get('Airline_Vistara'),
+            Airline_Vistara_Premium_economy=request.form.get('Airline_Vistara Premium economy'),
+            Source_Banglore=request.form.get('Source_Banglore'),
+            Source_Chennai=request.form.get('Source_Chennai'),
+            Source_Delhi=request.form.get('Source_Delhi'),
+            Source_Kolkata=request.form.get('Source_Kolkata'),
+            Source_Mumbai=request.form.get('Source_Mumbai'),
+            Destination_Banglore=request.form.get('Destination_Banglore'),
+            Destination_Cochin=request.form.get('Destination_Cochin'),
+            Destination_Delhi=request.form.get('Destination_Delhi'),
+            Destination_Hyderabad=request.form.get('Destination_Hyderabad'),
+            Destination_Kolkata=request.form.get('Destination_Kolkata'),
+            Destination_New_Delhi=request.form.get('Destination_New Delhi'),
+            Price = 0 # We need to set a default value for Price
         )
-        pred_df=data.get_data_as_data_frame()
+
+        pred_df = data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
+        predict_pipeline = PredictPipeline()
         print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
+        results = predict_pipeline.predict(pred_df)
+        print("After Prediction")
+        return render_template('home.html', results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,53 +8,67 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path=os.path.join("Data storage","model.pkl")
-            preprocessor_path=os.path.join('Data storage','proprocessor.pkl')
+            model_path = os.path.join("Data storage", "model.pkl")
+            preprocessor_path = os.path.join('Data storage', 'preprocessor.pkl')
             print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            model = utils.load_object(file_path=model_path)
+            preprocessor = utils.load_object(file_path=preprocessor_path)
             print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
             return preds
-        
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise exception.CustomException(e, sys)
 
 class CustomData:
-    def __init__( self,
-        #Airline: str,
-        Source: str,
-        Destination: str,
-        Total_Stops: str,
-        Date_of_Journey: str,
-        ):
-
-        self.Source = Source
-
-        self.Destination = Destination
-
+    def __init__(self,
+                 Total_Stops: str,
+                 month_of_journey: str,
+                 day_of_journey: str,
+                 Dep_Hour: str,
+                 Dep_Min: str,
+                 Duration_Total_Hour: str,
+                 Arrival_Hour: str,
+                 Arrival_Min: str,
+                 Airline: str,
+                 Source: str,
+                 Destination: str,
+                 Price: float):
+        
         self.Total_Stops = Total_Stops
-
-        self.Date_of_Journey = Date_of_Journey
-
-        #self.Airline = Airline
-
+        self.month_of_journey = month_of_journey
+        self.day_of_journey = day_of_journey
+        self.Dep_Hour = Dep_Hour
+        self.Dep_Min = Dep_Min
+        self.Duration_Total_Hour = Duration_Total_Hour
+        self.Arrival_Hour = Arrival_Hour
+        self.Arrival_Min = Arrival_Min
+        self.Airline = Airline
+        self.Source = Source
+        self.Destination = Destination
+        self.Price = Price
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                "Source": [self.Source],
-                "Destination": [self.Destination],
                 "Total_Stops": [self.Total_Stops],
-                "Date_of_Journey": [self.Date_of_Journey],
-                #"Airline": [self.Airline],
-                
+                "month_of_journey": [self.month_of_journey],
+                "day_of_journey": [self.day_of_journey],
+                "Dep_Hour": [self.Dep_Hour],
+                "Dep_Min": [self.Dep_Min],
+                "Duration_Total_Hour": [self.Duration_Total_Hour],
+                "Arrival_Hour": [self.Arrival_Hour],
+                "Arrival_Min": [self.Arrival_Min],
+                "Airline_" + self.Airline.replace(" ", "_"): [1],
+                "Source_" + self.Source.replace(" ", "_"): [1],
+                "Destination_" + self.Destination.replace(" ", "_"): [1],
+                "Price": [self.Price]
             }
 
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
-            raise CustomException(e, sys)
+            raise exception.CustomException(e, sys)
